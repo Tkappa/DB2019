acf034234c837bd563808679cd39085a630756fb
Tkappa <igor.ershov97@gmail.com>
2019-01-30 23:17:33 +0100

    Abbozzamento di sito per test

---
+++ site/html/operazione0.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>DB</title>
+  <link href="../css/bootstrap.css" rel="stylesheet">
+  <link href="../css/site.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">AlphaDB</a>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <p class="nav-link" href="index.html">Valentina Ferraioli</p>
+          </li>
+          <li class="nav-item">
+            <p class="nav-link" href="about.html">Igor Ershov</p>
+          </li>
+          <li class="nav-item">
+            <p class="nav-link" href="post.html">Stefano Andriolo</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container cbody">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="post-preview">
+          <h2 class="post-title">
+            Risultato
+          </h2>
+        </div>
+      </div>
+      <hr>
+    </div>
+
+    <div class="row">
+
+
+    </div>
+    <?php
+      $server = "golem.cs.unibo.it";
+      $username = "my1902";
+      $password = "Eech4Ieh";
+
+      $conn = new mysqli_connect($server,$username,$password,$username);
+
+      if ($conn->connect_error){
+      	die("Connection failed:" . $conn->connect_error);
+      }
+
+      $sql = $_POST["sql"];
+
+      echo("<p>".$sql."</p>")
+      $conn->query($sql);
+    ?>
+  </div>
+</body>
+</html>
